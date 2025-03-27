979005c98ab7640b16c0c92bb6aeb6c1dabd7fb5
Anna_Kharlamova <nutok87@gmail.com>
2025-03-27 10:56:29 +0100

    pipeline compile fix

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -30,6 +30,7 @@ public class Specifications {
         ));
         requestBuilder.setContentType(ContentType.JSON);
         requestBuilder.setAccept(ContentType.JSON);
+        requestBuilder.setBaseUri("http://" + Config.getProperty("host"));
         return requestBuilder;
     }
 
