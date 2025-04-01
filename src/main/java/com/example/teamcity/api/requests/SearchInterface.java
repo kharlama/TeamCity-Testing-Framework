01602e86e93810f40f2a65ad3fe86a49c3f0b8e4
Anna_Kharlamova <nutok87@gmail.com>
2025-04-01 11:17:33 +0200

    pipeline compile fix

--- src/main/java/com/example/teamcity/api/requests/SearchInterface.java
+++ src/main/java/com/example/teamcity/api/requests/SearchInterface.java
@@ -1,7 +1,5 @@
 package com.example.teamcity.api.requests;
 
-import com.example.teamcity.api.models.BaseModel;
-
 public interface SearchInterface {
     Object search(String searchParameter, String searchValue);
 }
